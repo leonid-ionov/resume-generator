73065b23d3590dab3cc82b688afbd570bd90a5a0
Leonid Ionov <leonid8ionov@gmail.com>
2024-03-04 00:22:10 +0400

    - Implement pre-push husky hook and update pre-commit hook.

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,6 +1,13 @@
-export default {
-  '*.{(m)?js,ts?(x),(s)?css,md}': filenames =>
-    `prettier --ignore-unknown --ignore-path .gitignore --write ${filenames.join(' ')}`,
-  '*.{(m)?js,ts?(x)}': filenames => `eslint --fix ${filenames.join(' ')}`,
-  '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
+const HookType = process.env.HUSKY_GIT_PARAMS;
+
+const configMap = {
+  'pre-commit': {
+    '*.{(m)?js,ts?(x),(s)?css,md}': filenames =>
+      `prettier --ignore-unknown --ignore-path .gitignore --write ${filenames.join(' ')}`,
+  },
+  'pre-push': {
+    '*.{(m)?js,ts?(x)}': filenames => `eslint --fix ${filenames.join(' ')}`,
+    '**/*.ts?(x)': () => 'tsc -p tsconfig.json --noEmit',
+  },
 };
+export default configMap[HookType];
